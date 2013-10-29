ee6f7bfe32ca30b4aa4cae56d34e106afcc2ea39
Konstantin Kostyuk <skystorm77@mail.ru>
2013-10-29 11:30:56 +0400

    motor control depending of time

--- Zumo_RC.ino
+++ Zumo_RC.ino
@@ -1,5 +1,5 @@
-// 22.10.2013 kkostyuk
-// Version 0.2 with motor control by external lib
+// 29.10.2013 kkostyuk
+// Version 0.3 motor control depending of time
 // External lib IRremote - https://github.com/shirriff/Arduino-IRremote
 #include <IRremote.h>
 #include <IRremoteInt.h>
@@ -10,7 +10,10 @@
 #define FW 0
 #define FW 1
 
-//#define MAXSPEED 50
+#define MAX_SPEED 100
+#define STEP_SPEED 20
+#define ACTION_DELEY 300
+#define STEP_DELEY 50
 
 //Pin config
 const int buttonPin = 2; //button 
@@ -21,21 +24,36 @@ decode_results results;
 
 //Motor
 ArduMoto Moto;
-byte motorAspeed = 0;
-byte motorBspeed = 0;
+int motorAspeed = 0;
+int motorBspeed = 0;
 
-byte changeSpeed(byte motorSpeed, byte change, byte dir)
+//Time
+long int currentTime = 0;
+long int actionTime = 0;
+long int diffTime = 0;
+
+//Get motor direction +motorSpeed = TRUE and -motorSpeed = False
+boolean getDir(int motorSpeed)
 {
-  boolean _dir = (motorSpeed < 0);
+  if (motorSpeed > 0)
+    return true;
+  else 
+    return false;
+}
+
+//Change motorSpeed to dalta with checks directions
+int changeSpeed(int currentSpeed, int deltaSpeed, int abs_maxSpeed)
+{ 
+  int newSpeed = currentSpeed; 
+  newSpeed += deltaSpeed;
+  if (currentSpeed >= 0){    
+    if (newSpeed > abs_maxSpeed) newSpeed = abs_maxSpeed;
+  }
+  else{
+    if (newSpeed < -abs_maxSpeed) newSpeed = -abs_maxSpeed;
+  };
   
-  byte _speed = abs(motorSpeed);
-  if (dir == FW) {
-    _speed += change;
-    return _speed;}
-  else {
-    _speed -= change;
-    return -_speed;}
-   
+  return newSpeed; 
 }
 
 
@@ -52,32 +70,42 @@ Serial.println("Init Complete.");
 }
 
 void loop() {
+  currentTime = millis();
 
   if (irrecv.decode(&results)) {
     Serial.println(results.value, HEX);
+    actionTime = currentTime + ACTION_DELEY;
    //MAin case
     switch(lowByte(results.value)){
     //Go forward
     case B00000011:
     case B00100000:
-      Moto.setSpeeds(100,100);    
-    Serial.println("Forward");
-    break;
+      motorAspeed=changeSpeed(max(motorAspeed,motorBspeed), STEP_SPEED, MAX_SPEED);
+      motorBspeed=changeSpeed(max(motorAspeed,motorBspeed), STEP_SPEED, MAX_SPEED);
+      Moto.setSpeeds(motorAspeed,motorBspeed);	  
+      Serial.println("Forward");
+   break;
     //Go rotate left
     case B00000001:
     case B00010000:
-      Moto.setSpeeds(0,50);
+      motorAspeed=changeSpeed(motorAspeed, -STEP_SPEED, MAX_SPEED);
+      motorBspeed=changeSpeed(motorBspeed, STEP_SPEED, MAX_SPEED);      
+      Moto.setSpeeds(motorAspeed,motorBspeed);
       Serial.println("Left");
     break;
     //Go rotate right
     case B00000010:
     case B00010001:
-      Moto.setSpeeds(50, 0);
+      motorAspeed=changeSpeed(motorAspeed, STEP_SPEED, MAX_SPEED);
+      motorBspeed=changeSpeed(motorBspeed, -STEP_SPEED, MAX_SPEED);      
+      Moto.setSpeeds(motorAspeed,motorBspeed);
       Serial.println("Right");
     break;
     //Go to back
     case B00100001:
-      Moto.setSpeeds(-50,-50);
+      motorAspeed=changeSpeed(min(motorAspeed,motorBspeed), -STEP_SPEED, MAX_SPEED);
+      motorBspeed=changeSpeed(min(motorAspeed,motorBspeed), -STEP_SPEED, MAX_SPEED);
+      Moto.setSpeeds(motorAspeed,motorBspeed);
       Serial.println("Back");
     break;
     //Stop    
@@ -85,12 +113,41 @@ void loop() {
     case B00111011:
       //stopBot();
       Moto.stop('A');
-      Moto.stop('B');      
+      motorAspeed=0;
+      Moto.stop('B');   
+      motorBspeed=0;   
       Serial.println("Stop");
     default:
       Serial.print("Incorrect comand: ");
       Serial.println(results.value, BIN);
     };
-    irrecv.resume(); // Receive the next value
+  irrecv.resume(); // Receive the next value
+      #ifdef DEBUG_MOTORS
+        Serial.print("Setting MotorA: ");
+        Serial.println(motorAspeed);
+        Serial.print("Setting MotorB: ");
+        Serial.println(motorBspeed);
+      #endif
   }  
-}
+  diffTime=actionTime-currentTime;
+  if (diffTime<0)
+    {
+     actionTime=currentTime+STEP_DELEY;
+     if (motorAspeed != 0){
+       if (motorAspeed > 0) motorAspeed=changeSpeed(motorAspeed, -1, MAX_SPEED);
+         else motorAspeed=changeSpeed(motorAspeed, 1, MAX_SPEED);
+       Moto.setSpeed('A',motorAspeed);
+     };     
+     if (motorBspeed != 0){
+       if (motorBspeed > 0) motorBspeed=changeSpeed(motorBspeed, -1, MAX_SPEED);
+         else motorBspeed=changeSpeed(motorBspeed, 1, MAX_SPEED);
+       Moto.setSpeed('B',motorBspeed);
+     };   
+      #ifdef DEBUG_MOTORS
+        Serial.print("Setting MotorA: ");
+        Serial.println(motorAspeed);
+        Serial.print("Setting MotorB: ");
+        Serial.println(motorBspeed);
+      #endif
+    } 
+ }
